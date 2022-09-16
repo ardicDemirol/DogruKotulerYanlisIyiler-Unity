b88143c4b63a4079d36f6e27d971b6c4993962cb
ArdicDemirol <ardc.dmrol@gmail.com>
2022-09-16 14:58:45 +0300

    cinemachine camera added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,13 +21,19 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        movement.x=  Input.GetAxisRaw("Horizontal");
+        movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        animator.SetFloat("Horizontal",movement.x);
+        animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
 
+        FlipCharcter();
+
+    }
+
+    private void FlipCharcter()
+    {
         if (movement.x < 0)
         {
             spriteRenderer.flipX = true;
@@ -36,16 +42,11 @@ public class PlayerMovement : MonoBehaviour
         {
             spriteRenderer.flipX = false;
         }
-
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-
-        //transform.rotation = Quaternion.Euler(0, 90*movement.x, 0);
-        
-       
-
+                  
     }
 }
