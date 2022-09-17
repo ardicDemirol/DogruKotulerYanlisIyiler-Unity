8b22e733ed34cd6662266bd61e431a428a7d9610
ArdicDemirol <ardc.dmrol@gmail.com>
2022-09-17 04:07:28 +0300

    gun improved

---
+++ Assets/Scripts/Enemy_Controller.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Controller : MonoBehaviour
+{
+    private Transform playerPos;
+    public float speed;
+    public int health = 5;
+
+    private void Start()
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Bullet")
+        {
+            health--;
+        }
+    }
+}
+
